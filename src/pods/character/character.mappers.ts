9d081d7d9e64d9ea493c346ed2c346855c66d35c
Aida Albarrán Blanco <aalbarranbla@aalbarranbl-a01.vmware.com>
2021-09-25 13:32:05 +0200

    Rename with character-collection and character

--- src/pods/character/character.mappers.ts
+++ src/pods/character/character.mappers.ts
@@ -1,9 +1,9 @@
-import * as apiModel from './api/hotel.api-model';
-import * as viewModel from './hotel.vm';
+import * as apiModel from './api/character.api-model';
+import * as viewModel from './character.vm';
 
-export const mapHotelFromApiToVm = (
-  hotel: apiModel.Hotel
-): viewModel.Hotel => ({
+export const mapCharacterFromApiToVm = (
+  hotel: apiModel.Character
+): viewModel.Character => ({
   ...hotel,
   id: hotel.id,
   name: hotel.name,
@@ -13,7 +13,7 @@ export const mapHotelFromApiToVm = (
   city: hotel.city,
 });
 
-export const mapHotelFromVmToApi = (hotel: viewModel.Hotel): apiModel.Hotel =>
+export const mapCharacterFromVmToApi = (hotel: viewModel.Character): apiModel.Character =>
   (({
     ...hotel,
     id: hotel.id,
@@ -22,4 +22,4 @@ export const mapHotelFromVmToApi = (hotel: viewModel.Hotel): apiModel.Hotel =>
     hotelRating: hotel.rating,
     address1: hotel.address,
     city: hotel.city,
-  } as unknown) as apiModel.Hotel);
+  } as unknown) as apiModel.Character);
