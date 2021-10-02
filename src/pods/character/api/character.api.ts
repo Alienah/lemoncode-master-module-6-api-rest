78b9c9e55330476a5462c229002ecde485cb6475
Aida Albarrán Blanco <aalbarranbla@aalbarranbl-a01.vmware.com>
2021-10-02 22:07:43 +0200

    add charactares mock and replace rick and morty api with mock-server

--- src/pods/character/api/character.api.ts
+++ src/pods/character/api/character.api.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import { Character } from './character.api-model';
 import { Lookup } from 'common/models';
 
-const URL = `https://rickandmortyapi.com/api/character`;
+const URL = '/api/characters';
 
 export const getCharacter = async (id: string): Promise<Character> => {
   const { data } = await Axios.get(`${URL}/${id}`);
